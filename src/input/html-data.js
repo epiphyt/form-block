001b97d159f59a324b307d207d84f1e75bd85bab
Matthias Kittsteiner <mk@kittmedia.com>
2023-01-27 21:15:55 +0100

    🏗️ Remove certain input types from block

--- src/input/html-data.js
+++ src/input/html-data.js
@@ -1,3 +1,5 @@
+import { applyFilters } from '@wordpress/hooks';
+
 export const getAllowedAttributes = ( type ) => {
 	return types[ type ].allowedAttributes;
 }
@@ -12,251 +14,107 @@ export const isAllowedAttribute = ( type, attribute ) => {
 	return types[ type ].allowedAttributes.includes( attribute );
 }
 
-const types = {
-	button: {
-		allowedAttributes: [
-			'disabled',
-			'readOnly',
-			'required',
-		],
+const types = applyFilters(
+	'formBlock.input.htmlTypes',
+	{
+		checkbox: {
+			allowedAttributes: [
+				'checked',
+				'disabled',
+				'label',
+				'required',
+			],
+		},
+		email: {
+			allowedAttributes: [
+				'autoComplete',
+				'disabled',
+				'label',
+				'maxLength',
+				'minLength',
+				'multiple',
+				'pattern',
+				'placeholder',
+				'readOnly',
+				'required',
+				'size',
+			],
+		},
+		file: {
+			allowedAttributes: [
+				'accept',
+				'autoComplete',
+				'capture',
+				'disabled',
+				'label',
+				'multiple',
+				'readOnly',
+				'required',
+			],
+		},
+		number: {
+			allowedAttributes: [
+				'autoComplete',
+				'disabled',
+				'label',
+				'max',
+				'min',
+				'placeholder',
+				'readOnly',
+				'required',
+				'step',
+			],
+		},
+		radio: {
+			allowedAttributes: [
+				'checked',
+				'disabled',
+				'label',
+				'required',
+			],
+		},
+		reset: {
+			allowedAttributes: [
+				'autoComplete',
+				'disabled',
+				'label',
+				'readOnly',
+				'required',
+			],
+		},
+		submit: {
+			allowedAttributes: [
+				'disabled',
+			],
+		},
+		tel: {
+			allowedAttributes: [
+				'autoComplete',
+				'disabled',
+				'label',
+				'maxLength',
+				'minLength',
+				'pattern',
+				'placeholder',
+				'readOnly',
+				'required',
+				'size',
+			],
+		},
+		text: {
+			allowedAttributes: [
+				'autoComplete',
+				'dirname',
+				'disabled',
+				'label',
+				'maxLength',
+				'minLength',
+				'pattern',
+				'placeholder',
+				'readOnly',
+				'required',
+				'size',
+			],
+		},
 	},
-	checkbox: {
-		allowedAttributes: [
-			'checked',
-			'disabled',
-			'label',
-			'required',
-		],
-	},
-	color: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-		],
-	},
-	date: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'max',
-			'min',
-			'readOnly',
-			'required',
-			'step',
-		],
-	},
-	'datetime-local': {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'max',
-			'min',
-			'readOnly',
-			'required',
-			'step',
-		],
-	},
-	email: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'maxLength',
-			'minLength',
-			'multiple',
-			'pattern',
-			'placeholder',
-			'readOnly',
-			'required',
-			'size',
-		],
-	},
-	file: {
-		allowedAttributes: [
-			'accept',
-			'autoComplete',
-			'capture',
-			'disabled',
-			'label',
-			'multiple',
-			'readOnly',
-			'required',
-		],
-	},
-	hidden: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-		],
-	},
-	image: {
-		allowedAttributes: [
-			'alt',
-			'autoComplete',
-			'disabled',
-			'height',
-			'label',
-			'readOnly',
-			'required',
-			'src',
-			'width',
-		],
-	},
-	month: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'max',
-			'min',
-			'readOnly',
-			'required',
-			'step',
-		],
-	},
-	number: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'max',
-			'min',
-			'placeholder',
-			'readOnly',
-			'required',
-			'step',
-		],
-	},
-	password: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'maxLength',
-			'minLength',
-			'pattern',
-			'placeholder',
-			'readOnly',
-			'required',
-			'size',
-		],
-	},
-	radio: {
-		allowedAttributes: [
-			'checked',
-			'disabled',
-			'label',
-			'required',
-		],
-	},
-	range: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'max',
-			'min',
-			'step',
-		],
-	},
-	reset: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'readOnly',
-			'required',
-		],
-	},
-	search: {
-		allowedAttributes: [
-			'autoComplete',
-			'dirname',
-			'disabled',
-			'label',
-			'maxLength',
-			'minLength',
-			'pattern',
-			'placeholder',
-			'readOnly',
-			'required',
-			'size',
-		],
-	},
-	submit: {
-		allowedAttributes: [
-			'disabled',
-		],
-	},
-	tel: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'maxLength',
-			'minLength',
-			'pattern',
-			'placeholder',
-			'readOnly',
-			'required',
-			'size',
-		],
-	},
-	text: {
-		allowedAttributes: [
-			'autoComplete',
-			'dirname',
-			'disabled',
-			'label',
-			'maxLength',
-			'minLength',
-			'pattern',
-			'placeholder',
-			'readOnly',
-			'required',
-			'size',
-		],
-	},
-	time: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'max',
-			'min',
-			'readOnly',
-			'required',
-			'step',
-		],
-	},
-	url: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'maxLength',
-			'minLength',
-			'pattern',
-			'placeholder',
-			'readOnly',
-			'required',
-			'size',
-		],
-	},
-	week: {
-		allowedAttributes: [
-			'autoComplete',
-			'disabled',
-			'label',
-			'max',
-			'min',
-			'readOnly',
-			'required',
-			'step',
-		],
-	},
-};
+);
