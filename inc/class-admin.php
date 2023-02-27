5d431c1f432ab9cb574fd64652f7315eb60c048d
Matthias Kittsteiner <mk@kittmedia.com>
2023-02-27 10:18:57 +0100

    :bento: Rename editor script handle

--- inc/class-admin.php
+++ inc/class-admin.php
@@ -35,7 +35,7 @@ final class Admin {
 	public function block_assets(): void {
 		$asset_file = include plugin_dir_path( EPI_FORM_BLOCK_FILE ) . 'build/index.asset.php';
 		
-		wp_enqueue_script( 'form-block', plugin_dir_url( EPI_FORM_BLOCK_FILE ) . 'build/index.js', $asset_file['dependencies'], $asset_file['version'] );
+		wp_enqueue_script( 'form-block-editor', plugin_dir_url( EPI_FORM_BLOCK_FILE ) . 'build/index.js', $asset_file['dependencies'], $asset_file['version'] );
 		wp_enqueue_style( 'form-block', plugin_dir_url( EPI_FORM_BLOCK_FILE ) . 'build/style-index.css', [], $asset_file['version'] );
 	}
 	
