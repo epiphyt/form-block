19d5484b4a28fcc0ea1fecab205cb43afdf22c49
Matthias Kittsteiner <mk@kittmedia.com>
2023-03-18 21:14:27 +0100

    ✨ Add maximum upload size setting

--- inc/class-admin.php
+++ inc/class-admin.php
@@ -26,6 +26,7 @@ final class Admin {
 	 * Initialize the class.
 	 */
 	public function init(): void {
+		add_action( 'admin_init', [ $this, 'register_options' ] );
 		add_action( 'enqueue_block_editor_assets', [ $this, 'block_assets' ] );
 	}
 	
@@ -50,5 +51,85 @@ final class Admin {
 		}
 		
 		return self::$instance;
+	}
+	
+	/**
+	 * Get the input for maximum upload size option.
+	 */
+	public function get_maximum_upload_size_input(): void {
+		$maximum_upload_size = floor( wp_max_upload_size() / 1024 / 1024 * 100 ) / 100;
+		$option_value = get_option( 'form_block_maximum_upload_size' );
+		?>
+		<input type="number" id="form_block_maximum_upload_size" name="form_block_maximum_upload_size" value="<?php echo esc_attr( $option_value ); ?>" step=".01" min="0" max="<?php echo esc_attr( $maximum_upload_size ); ?>" class="small-text" /> <?php esc_html_e( 'MiB', 'form-block' ); ?>
+		<p>
+			<?php
+			/* translators: upload size limit */
+			printf( esc_html__( 'Your server is capable of uploads in size of %s MiB. Please keep in mind that your mail server could have other (lower) limits.', 'form-block' ), esc_html( number_format_i18n( $maximum_upload_size, 2 ) ) );
+			?>
+		</p>
+		<?php
+	}
+	
+	/**
+	 * Register options.
+	 */
+	public function register_options(): void {
+		add_settings_section(
+			'form_block',
+			esc_html__( 'Form Block', 'form-block' ),
+			null,
+			'writing',
+		);
+		add_settings_field(
+			'form_block_maximum_upload_size',
+			__( 'Maximum form upload size', 'form-block' ),
+			[ $this, 'get_maximum_upload_size_input' ],
+			'writing',
+			'form_block',
+		);
+		register_setting(
+			'writing',
+			'form_block_maximum_upload_size',
+			[
+				'sanitize_callback' => [ $this, 'validate_maximum_upload_size' ],
+				'type' => 'number',
+			]
+		);
+	}
+	
+	/**
+	 * Validate maximum upload size setting.
+	 * 
+	 * @param	string	$value The saved value
+	 * @return	string The validated value
+	 */
+	public function validate_maximum_upload_size( string $value ): string {
+		if ( ! is_numeric( $value ) ) {
+			add_settings_error(
+				'form_block_maximum_upload_size',
+				'invalid_value',
+				/* translators: setting name */
+				sprintf( esc_html__( '%s: The entered value is invalid.', 'form-block' ), esc_html__( 'Maximum form upload size', 'form-block' ),
+				)
+			);
+			
+			return '';
+		}
+		
+		$byte_value = floor( $value * 1024 * 1024 );
+		
+		if ( $byte_value > wp_max_upload_size() ) {
+			add_settings_error(
+				'form_block_maximum_upload_size',
+				'invalid_value',
+				/* translators: setting name */
+				sprintf( esc_html__( '%s: The value must not be greater than the maximum upload size the server is capable of.', 'form-block' ), esc_html__( 'Maximum form upload size', 'form-block' ),
+				)
+			);
+			
+			return '';
+		}
+		
+		return $value;
 	}
 }
