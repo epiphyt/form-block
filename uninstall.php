c9ae0bef0d5c82bf57dadb97f0004fb6e9ab05c4
Matthias Kittsteiner <mk@kittmedia.com>
2023-03-19 14:43:38 +0100

    ✨ Add deleting data on uninstall

---
+++ uninstall.php
@@ -0,0 +1,51 @@
+<?php
+namespace epiphyt\Form_Block;
+
+// if uninstall.php is not called by WordPress, die
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+$GLOBALS['options'] = [
+	'form_block_form_ids',
+	'form_block_maximum_upload_size',
+	'form_block_preserve_data_on_uninstall',
+];
+
+if ( is_multisite() ) {
+	$sites = get_sites( [ 'number' => 99999 ] );
+	
+	foreach ( $sites as $site ) {
+		switch_to_blog( $site->blog_id );
+		
+		// do nothing if option says so
+		if ( get_option( 'form_block_preserve_data_on_uninstall' ) ) {
+			continue;
+		}
+		
+		delete_data();
+		restore_current_blog();
+	}
+}
+else if ( ! get_option( 'form_block_preserve_data_on_uninstall' ) ) {
+	delete_data();
+}
+
+/**
+ * Delete all data.
+ */
+function delete_data() {
+	global $options;
+	
+	foreach ( $options as $option ) {
+		if ( $option === 'form_block_form_ids' ) {
+			$form_ids = get_option( $option );
+			
+			foreach ( $form_ids as $form_id => $object_ids ) {
+				delete_option( 'form_block_data_' . $form_id );
+			}
+		}
+		
+		delete_option( $option );
+	}
+}
