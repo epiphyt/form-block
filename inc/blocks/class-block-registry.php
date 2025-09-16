321a87d45ab4a452ea23248df74548db785760b3
Matthias Kittsteiner <mk@kittmedia.com>
2025-09-16 22:32:05 +0200

    ✨ Allow using block registry from outside See #61

--- inc/blocks/class-block-registry.php
+++ inc/blocks/class-block-registry.php
@@ -20,28 +20,31 @@ final class Block_Registry {
 	
 	/**
 	 * Register blocks.
+	 * 
+	 * @param	string	$base_path Plugin base path
 	 */
-	public function register(): void {
+	public function register( string $base_path = '' ): void {
+		if ( empty( $base_path ) ) {
+			$base_path = \EPI_FORM_BLOCK_BASE;
+		}
+		
 		if ( \function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
 			// WordPress 6.8+
 			\wp_register_block_types_from_metadata_collection(
-				\EPI_FORM_BLOCK_BASE . 'build',
-				\EPI_FORM_BLOCK_BASE . 'build/blocks-manifest.php'
+				$base_path . 'build',
+				$base_path . 'build/blocks-manifest.php'
 			);
 		}
 		else {
 			// WordPress 6.7
 			if ( \function_exists( 'wp_register_block_metadata_collection' ) ) {
-				\wp_register_block_metadata_collection(
-					\EPI_FORM_BLOCK_BASE . 'build',
-					\EPI_FORM_BLOCK_BASE . 'build/blocks-manifest.php'
-				);
+				\wp_register_block_metadata_collection( $base_path . 'build', $base_path . 'build/blocks-manifest.php' );
 			}
 			
-			$manifest_data = require \EPI_FORM_BLOCK_BASE . 'build/blocks-manifest.php';
+			$manifest_data = require $base_path . 'build/blocks-manifest.php';
 			
 			foreach ( \array_keys( $manifest_data ) as $block_type ) {
-				\register_block_type( \EPI_FORM_BLOCK_BASE. "build/{$block_type}" );
+				\register_block_type( $base_path. "build/{$block_type}" );
 			}
 		}
 	}
